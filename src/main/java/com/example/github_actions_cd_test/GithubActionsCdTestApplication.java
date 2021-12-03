1cb7cbae13b6a46788f0672e22f30a9d1f67c4af
Seongbin Kim <seongbin9786@gmail.com>
2021-12-03 21:51:08 +0900

    init gradle-spring

---
+++ src/main/java/com/example/github_actions_cd_test/GithubActionsCdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_cd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsCdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsCdTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/github_actions_cd_test/GithubActionsCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_cd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsCdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
